8264721d7e0fed10d39661baf7d6a8c605997205
Kohl <marcelokohl@gmail.com>
2018-10-01 19:13:49 -0300

    wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    app
+    app 2
     <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-// import Home from './pages/home.vue'
+import Home from './pages/Home.vue'
 
 export default {
   name: 'app',
   components: {
-    // Home
+    Home
   }
 }
 </script>
